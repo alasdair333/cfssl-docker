1bb3249f838501c1df0cea84249fdb053eba3a01
Alasdair MacLeod <alasdair333@users.noreply.github.com>
2020-12-20 14:09:20 +0000

    removed env vars

--- main.tf
+++ main.tf
@@ -35,10 +35,6 @@ resource "null_resource" "cfssl_docker" {
         command = "${path.module}/create_service.sh"
 
         environment = {
-            CN=var.common_name
-            ATTRS=tostring(jsonencode(var.attributes))
-            KEY_SETTINGS=tostring(jsonencode(var.key_settings))
-            API_KEY=var.api_key
             CONTEXT=var.docker_context
             MODULE_DIR=path.module
         }
